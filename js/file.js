246f35a3415346e81b740cfc4518872a7db9aa3b
Jose Juarez <juniojuarez@yahoo.com.br>
2025-01-07 18:31:32 -0300

    Criada função de importar arquivos via txt

---
+++ js/file.js
@@ -0,0 +1,106 @@
+const inputFile = document.querySelector("#arquivo-cep");
+const prewviewCeps = document.querySelector("#prewviewCeps");
+const validarListaCep = document.querySelector("#validarListaCep");
+
+document.querySelector("arquivoExemplo");
+
+const conteudoArquivo = [
+  "39403-076",
+  "0100-1000",
+  "0201-2000",
+  "03021000",
+  "04023000",
+  "05047000",
+  "06059000",
+];
+
+arquivoExemplo.addEventListener("click", () => {
+  const blob = new Blob([conteudoArquivo], { type: "text/plain" });
+
+  const link = document.createElement("a");
+  const url = URL.createObjectURL(blob);
+
+  link.href = url;
+
+  link.download = "exemplo.txt";
+
+  link.click();
+  window.URL.revokeObjectURL(url);
+});
+
+let listaCeps = document.querySelector("#listaCeps");
+let cepsArrray = [];
+
+inputFile.addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  function isValidTxt(file) {
+    const isTxtExtesion = file.name.endsWith(".txt");
+    const isMimeType = file.type === "text/plains";
+
+    return isTxtExtesion;
+  }
+
+  if (file) {
+    if (isValidTxt(file)) {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        const content = reader.result.split(",");
+        cepsArrray = content.map((item) =>
+          item.replace("-", "").replace(" ", "")
+        );
+        console.log(cepsArrray);
+        prewviewCeps.innerHTML = "";
+        prewviewCeps.innerHTML = `
+        <pre>${cepsArrray}</pre> 
+         <button type="submit" id="limpaPreview">Limpar</button>
+        `;
+        const limpaPreview = document.querySelector("#limpaPreview");
+
+        limpaPreview.addEventListener("click", () => {
+          prewviewCeps.innerHTML = "";
+        });
+      });
+      reader.readAsText(file);
+    } else {
+      alert("Arquivo precisa ser do tipo '.txt'");
+    }
+  } else {
+    alert("nenhum aruqivo importado");
+  }
+});
+
+async function validaListaCep(cep) {
+  let urlViaCEP = `https://viacep.com.br/ws/${cep}/json/`;
+  let response = await fetch(urlViaCEP);
+  let json = await response.json();
+
+  if (json.erro) {
+    listaCeps.innerHTML = `<p>O CEP ${cepValue} não é mais valido.</p>`;
+  } else {
+    listaCeps.innerHTML += `
+
+    <ul id="ceps-lista-ul">
+      <li><strong>CEP:</strong> ${json.cep}</li>
+      <li><strong>Logradouro:</strong> ${json.logradouro}</li>
+      <li><strong>Complemento:</strong> ${json.complemento}</li>
+      <li><strong>Bairro:</strong> ${json.bairro}</li>
+      <li><strong>Localidade:</strong> ${json.localidade}</li>
+      <li><strong>UF:</strong> ${json.uf}</li>
+      <li><strong>Estado:</strong> ${json.estado}</li>
+      <li><strong>Região:</strong> ${json.regiao}</li>
+      <li><strong>DDD:</strong> ${json.ddd}</li>
+    </ul>
+    <hr/>
+    `;
+  }
+}
+
+validarListaCep.addEventListener("click", () => {
+  if (cepsArrray.length > 0) {
+    for (let index = 0; index <= 10; index++) {
+      validaListaCep(cepsArrray[index]);
+    }
+  } else {
+    alert("Nenhum arquivo importado");
+  }
+});
